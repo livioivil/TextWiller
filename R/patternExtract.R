26fa5c1a5fe74ad0dc3df96c09e7a1f0e2871cef
livioivil <livio@stat.unipd.it>
2014-01-28 11:06:56 +0100

    aggiunta funzione patternExtract()

---
+++ R/patternExtract.R
@@ -0,0 +1,12 @@
+patternExtract <- function(testo, pattern="@\\w+", id = names(testo)){
+  
+  if(is.null(id)) id=1:length(testo)
+     pattern <- str_extract_all(testo, pattern)
+  names(pattern)=id
+  
+  pattern=pattern[sapply(pattern,length)>0]
+  db.pattern=lapply(1:length(pattern),function(i){ rbind(id=names(pattern)[i],pattern=unlist(pattern[i]))})
+  db.pattern=t(data.frame(db.pattern))
+  rownames(db.pattern)=NULL
+  return(db.pattern)	
+}
