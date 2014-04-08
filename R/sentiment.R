82a1e88cb228c542be055c9c5fc6c405868a16fd
maddalenaB <mariamaddalena.branca@gmail.com>
2014-04-08 16:27:29 +0200

    aggiunta codice maddalena

--- R/sentiment.R
+++ R/sentiment.R
@@ -2,20 +2,37 @@ sentiment <- function(text, algorithm="Maddalena", vocabularies=NULL){
   if(!is.null(text)){ #se c'e' almeno un testo
     if(is.null(algorithm)) algorithm="Maddalena"
     if(is.null(vocabularies)) {
-      #       data(pos) #lo decommenti ed eventualmente cambi nomi dei dati 
-      #       data(neg) 
-      #       vocabularies =list(pos=pos,neg=neg)
-      #     per ora:
-      vocabularies =list(pos="pos",neg="neg")
+      # data(sentimentVocabularies) #lo decommenti ed eventualmente cambi nomi dei dati
+      load("../data/dizionariMadda.rda")
+      vocabularies=sentimentVocabularies
     }
     
     #choose and perform algorithm
     if(algorithm=="Maddalena") {
       sent=.sentiment.maddalena(text=text, vocabularies=vocabularies)
       return(sent)
-      } else NULL
+    } else NULL
   } #end if(!is.null(text))
 }
 
-# ci sostituisca la sua funzione:
-.sentiment.maddalena <- function(text, vocabularies){rep(c("pos","neg","neutro"), length.out=length(text))}
+.sentiment.maddalena<- function(text, vocabularies){
+  
+  source("funzioniMadda.R")
+  
+  #pos.words=lapply((vocabularies$positive),SnowballStemmer,control=Weka_control(S="italian"))
+  #neg.words=lapply((vocabularies$negative),SnowballStemmer,control=Weka_control(S="italian"))
+  pos.words=lapply((vocabularies$positive),wordStem,language="italian")
+  neg.words=lapply((vocabularies$negative),wordStem,language="italian")
+  
+  pos.words = unique(pos.words)
+  neg.words = unique(neg.words)
+  
+  tweet_array2<-pulizia(text)
+  
+  
+  #####################################################sentiment analisys
+  result1= score.sentiment((tweet_array2), pos.words, neg.words) #,p.neutral.words,n.neutral.words)
+  #write.table(result1[1:30,],"risultatiMio.txt")
+  
+  
+}
