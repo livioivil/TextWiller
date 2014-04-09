c08806d1d23d360ad3ba7c86cf84faf120297e58
livioivil <livio@stat.unipd.it>
2014-04-09 08:36:10 +0200

    debuging setiment() & co.

--- R/sentiment.R
+++ R/sentiment.R
@@ -14,17 +14,3 @@ sentiment <- function(text, algorithm="Maddalena", vocabularies=NULL){
   } #end if(!is.null(text))
 }
 
-.sentiment.maddalena<- function(text, vocabularies){
-  #pos.words=lapply((vocabularies$positive),SnowballStemmer,control=Weka_control(S="italian"))
-  #neg.words=lapply((vocabularies$negative),SnowballStemmer,control=Weka_control(S="italian"))
-#   pos.words=lapply((vocabularies$positive),wordStem,language="italian")
-#   neg.words=lapply((vocabularies$negative),wordStem,language="italian")
-  
-  pos.words = unique(pos.words)
-  neg.words = unique(neg.words)
-  
-  tweet_array2<-normalizzaTesti(text,preprocessingEncoding=FALSE)$testo
-  #####################################################sentiment analisys
-  result1= score.sentiment((tweet_array2), pos.words, neg.words) #,p.neutral.words,n.neutral.words)
-  #write.table(result1[1:30,],"risultatiMio.txt") 
-}
