455a9e55f745ed6b8e802270307d260cc597c962
livioivil <livio@stat.unipd.it>
2014-04-10 10:01:14 +0200

    debuging normalizzaTesti()

--- R/funzioniMadda.R
+++ R/funzioniMadda.R
@@ -1,5 +1,5 @@
 .sentiment.maddalena<- function(text, vocabularies){
-  text<-normalizzaTesti(text,suppressInvalidTexts=FALSE,conteggiStringhe=NULL)$testo
+  text<-normalizzaTesti(text,suppressInvalidTexts=FALSE,contaStringhe=NULL)$testo
   #####################################################sentiment analisys
   
 # sfInit(par=T,cp=1) #cp = 10 calculus, devo metterci il numero di processori 
