4fec37eb1c23cb7e511cb19d78bb14a79d960767
Paolonadalutti <paolo.nadalutti@gmail.com>
2014-07-07 18:01:44 +0200

    Update normalizzaPunteggiatura_tm.R
    Versione "tm oriented" di normalizzapunteggiatura

--- R/normalizzaPunteggiatura_tm.R
+++ R/normalizzaPunteggiatura_tm.R
@@ -27,7 +27,7 @@ charnorm <- function(corpus,fixed=TRUE){
 	corpus <- tm_map(corpus, function(t)  gsub("\n"," ", corpus, fixed=fixed, useBytes=FALSE))
 	corpus <- tm_map(corpus, function(t)  gsub("\r"," ", corpus, fixed=fixed, useBytes=FALSE))
 	corpus <- tm_map(corpus, function(t)  gsub("\t"," ", corpus, fixed=fixed, useBytes=FALSE))
-	#Nuove righe di codice aggiunte su altri caratteri da normalizzare
+	#Nuove righe di codice aggiunte su altri caratteri da normalizzare.
 	corpus <- tm_map(corpus, function(t) gsub(" e\' "," è ",t,fixed=TRUE))
 	corpus <- tm_map(corpus, function(t) gsub("a\' ","à ",t,fixed=TRUE))
 	corpus <- tm_map(corpus, function(t) gsub("u\' ","ù ",t,fixed=TRUE))
