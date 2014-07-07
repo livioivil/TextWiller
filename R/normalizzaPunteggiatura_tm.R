c05476af05dc21c13412aa35c62c744de42d5cd7
Paolonadalutti <paolo.nadalutti@gmail.com>
2014-07-07 18:02:57 +0200

    Update normalizzaPunteggiatura_tm.R

--- R/normalizzaPunteggiatura_tm.R
+++ R/normalizzaPunteggiatura_tm.R
@@ -27,15 +27,6 @@ charnorm <- function(corpus,fixed=TRUE){
 	corpus <- tm_map(corpus, function(t)  gsub("\n"," ", corpus, fixed=fixed, useBytes=FALSE))
 	corpus <- tm_map(corpus, function(t)  gsub("\r"," ", corpus, fixed=fixed, useBytes=FALSE))
 	corpus <- tm_map(corpus, function(t)  gsub("\t"," ", corpus, fixed=fixed, useBytes=FALSE))
-	#Nuove righe di codice aggiunte su altri caratteri da normalizzare.
-	corpus <- tm_map(corpus, function(t) gsub(" e\' "," è ",t,fixed=TRUE))
-	corpus <- tm_map(corpus, function(t) gsub("a\' ","à ",t,fixed=TRUE))
-	corpus <- tm_map(corpus, function(t) gsub("u\' ","ù ",t,fixed=TRUE))
-	corpus <- tm_map(corpus, function(t) gsub("i\' ","ì ",t,fixed=TRUE))
-	corpus <- tm_map(corpus, function(t) gsub("[-\']"," ",t))
-	corpus <- tm_map(corpus, function(t) gsub(" +"," ",t))
-	corpus <- tm_map(corpus, function(t) gsub("á","à",t,fixed=TRUE))
-	corpus <- tm_map(corpus, function(t) gsub("ó","ò",t,fixed=TRUE))
 
   corpus
 }
