7cba6fd626bac4dc82eb043edbb25d176cd36e26
livio <livio@stat.unipd.it>
2015-02-08 19:24:59 +0100

    normalizza emote a testi

--- R/normalizzaTesti.R
+++ R/normalizzaTesti.R
@@ -1,58 +1,49 @@
+
 normalizzaTesti <- function(testo, tolower=TRUE,normalizzahtml=TRUE,
-                             normalizzacaratteri=TRUE,fixed=TRUE,perl=TRUE,
-                             preprocessingEncoding=TRUE, encoding="UTF-8", sub="",
+                            normalizzacaratteri=TRUE,
+                            normalizzaemote=TRUE,
+                            normalizzapunteggiatura=TRUE,
+                            normalizzaslang=TRUE,
+                            fixed=TRUE,perl=TRUE,
+                            preprocessingEncoding=TRUE, encoding="UTF-8", sub="",
                             contaStringhe=c("\\?","\\!","@","#",
-                                           "(\u20AC|euro)","(\\$|dollar)",
-                                           "SUPPRESSEDTEXT"),
+                                            "(\u20AC|euro)","(\\$|dollar)",
+                                            "SUPPRESSEDTEXT"),
                             suppressInvalidTexts=TRUE,
-                             verbatim=TRUE, remove=TRUE){
+                            verbatim=TRUE, remove=TRUE){
   Sys.setlocale("LC_ALL", "")
-  if(preprocessingEncoding)  testo<-preprocessingEncoding(testo,
-                                      encoding=encoding,
-                                      sub=sub, 
-                                      suppressInvalidTexts=suppressInvalidTexts,
-                                      verbatim=verbatim)
-	#######################
-	#   PREPROCESSING     #
-	#######################
-  
+  if(preprocessingEncoding) testo<-preprocessingEncoding(testo,
+                                                         encoding=encoding,
+                                                         sub=sub,
+                                                         suppressInvalidTexts=suppressInvalidTexts,
+                                                         verbatim=verbatim)
+  #######################
+  # PREPROCESSING #
+  #######################
   # aggiunta spazi per preprocessing
   testo <- paste(" ",testo," ",sep="")
-  
-  
   # normalizza encoding
   if(normalizzacaratteri) testo <- normalizzacaratteri(testo,fixed=fixed)
-  
   # pulizia testo preliminare (html)
   if(normalizzahtml) testo <- normalizzahtml(testo)
-  
-  
-  if(!is.null(contaStringhe)) 
-    conteggiStringhe=.contaStringhe(testo,contaStringhe) else 
-    conteggiStringhe=NULL
-	# identifica emote
-	#source(paste(functiondir,"/normalizzaemote.R",sep=""), .GlobalEnv)
-	testo <- normalizzaemote(testo,perl=perl)
-
-	# pulizia punteggiatura
-	#source(paste(functiondir,"/normalizzapunteggiatura.R",sep=""), .GlobalEnv)
-	testo <- normalizzapunteggiatura(testo,perl=perl,fixed=fixed)
-
-  
-	# normalizza slang 
-	#source(paste(functiondir,"/normalizzaslang.R",sep=""), .GlobalEnv)
-	testo <- normalizzaslang(testo,perl=perl)
-
+  if(!is.null(contaStringhe))
+    conteggiStringhe=.contaStringhe(testo,contaStringhe) else
+      conteggiStringhe=NULL
+  # identifica emote
+  #source(paste(functiondir,"/normalizzaemote.R",sep=""), .GlobalEnv)
+  if(normalizzaemote) testo <- normalizzaemote(testo,perl=perl)
+  # pulizia punteggiatura
+  #source(paste(functiondir,"/normalizzapunteggiatura.R",sep=""), .GlobalEnv)
+  if(normalizzapunteggiatura) testo <- normalizzapunteggiatura(testo,perl=perl,fixed=fixed)
+  # normalizza slang
+  #source(paste(functiondir,"/normalizzaslang.R",sep=""), .GlobalEnv)
+  if(normalizzaslang) testo <- normalizzaslang(testo,perl=perl)
   # tolower
-  if(tolower)  testo <- tryTolower(testo,ifErrorReturnText=TRUE)
-  
+  if(tolower) testo <- tryTolower(testo,ifErrorReturnText=TRUE)
   data(itastopwords)
   if(remove) testo <- removeStopwords(testo)
-    
   testo <- gsub("\\s+", " ", testo, perl=perl)
   testo <- .togliSpaziEsterni(testo)
   attr(testo,"counts")=conteggiStringhe
-  
-  
   testo
 }
--- R/normalizzaemote.R
+++ R/normalizzaemote.R
@@ -1,43 +1,36 @@
 normalizzaemote <-
-function(testo,perl=TRUE){
-	# EMOTEGOOD :) :-) :] :-] =) =] => :> ^^ ^_^ ^-^ ^o^ : ) (: :'D
-testo <- gsub("\\:\\)+|\\:\\-\\)+|\\:\\]+|\\:\\-\\]+|\\=\\)+|\\=\\]+|\\=\\>|\\:\\>|\\^\\^|\\^\\_+\\^|\\^\\-\\^|\\^o\\^|\\:[[:blank:]]\\)+|[[:blank:]]\\([[:blank:]]?\\:|\\:\\'D+",' EMOTEGOOD ', testo, perl=perl)	
-
-#testo <- gsub(":\\-?[\\)\\]]+",' EMOTEGOOD ', testo, perl=TRUE)	
-
-
-	# EMOTEGOOD :d :D :-d :-D =d =D 8d 8D :') 
-testo <- gsub("\\:d+|\\:D+|\\:\\-d+|\\:\\-D+|\\=d+|\\=D+|8d+|8D+|\\:\\'+\\)+|v\\.v",' EMOTEGOOD ', testo, perl=perl)
-	# EMOTELOVE <3 :*
-testo <- gsub("\\<3+|\u2764|\u2665|\\:\\*+",' EMOTELOVE ', testo, perl=perl)        
-              # EMOTEBAD :( :-( :[ :-[ =[ =( : ( ):
-testo <- gsub("\\:\\(+|\\:\\-\\(+|\\:\\[+|\\:\\-\\[+|\\=\\[+|\\=\\(+|\\:[[:blank:]]\\(|[[:blank:]]\\([[:blank:]]?\\:",' EMOTEBAD ', testo, perl=perl)
-	# EMOTEBAD :'( :-[ D:
-testo <- gsub("\\:\\'+\\(+|\\:\\'\\[|D\\:|\\:\\-\\[",' EMOTEBAD ', testo, perl=perl)
-	# EMOTEBAD :| :/ =/ :x :-|
-testo <- gsub("\\:\\|\\:/+|\\=/+|\\:x",' EMOTEBAD ', testo, perl=perl)
-	# EMOTEBAD #_# X_X x_x X.X x.x >.< >_< >.> >_>
-testo <- gsub("\\#\\_+\\#|X\\_+X|x\\_+x|X\\.X|x\\.x|>\\.<|>\\_+<|>\\_+>|>\\.>",' EMOTEBAD ', testo, perl=perl)
-	# EMOTEWINK ;) ;-) ;] ;-] ;> ;d ;D ;o
-testo <- gsub("\\;\\)+|\\;\\-\\)+|\\;\\]|\\;\\-\\]|\\;\\>|;d+|;D+|;o",' EMOTEWINK ', testo, perl=perl)
-	# EMOTESHOCK O.o o.o O.O o.O O_o o_o O_O o_O etc
-testo <- gsub("O\\.o|o\\.o|O\\.O|o\\.O|O\\_+o|o\\_+o|O\\_+O|o\\_+O|\\:OO+|\\=O+|\\-\\.\\-|u\\.u|u\\.\u00F9|\u00F9\\.u|u\\_+u|\u00E7\u00E7|\u00E7_+\u00E7|t_+t|\u00F9\\_+\u00F9|\u00F9\\.\u00F9|\\:oo+|0\\_+0|\\=\\_+\\=|\\.\\_+\\.|\u00F2\u00F2|\u00F2\\_+\u00F2|\\*u+\\*|\\-\\_+\\-|\u00F9\u00F9|\\-\\,\\-|\\-\\-\\'|\\.\\-\\.|\\'\\-\\'",' EMOTESHOCK ', testo, perl=perl)
-	# EMOTEAMAZE *_* *-* *o* *.* 
-testo <- gsub("\\*\\_+\\*|\\*\\-\\*|\\*\\o\\*|\\*\\.\\*",' EMOTEAMAZE ', testo, perl=perl)
-	# EMOTEJOKE :P :p =P =p XD xD xd d:
-testo <- gsub("\\:P+[^e]|\\:p+[^e]|\\=P+|\\=p+|XD+|xD+|xd+|[[:blank:]]d\\:",' EMOTEAMAZE ', testo, perl=perl)
-
-	# NEW VERSION
-#testo <- gsub("([:=8]([- '])?[])Dd>]+)|(\\^[-_o]?\\^)","EMOTEGOOD", testo)
-#testo <- gsub("([:=]([- '])?[(|/x*[])|([>Xx#][._][>Xx<#])|(\\):)","EMOTEBAD", testo)
-#testo <- gsub(";-?[])>Ddo]","EMOTEWINK", testo)
-#testo <- gsub("\\*[-._o]\\*","EMOTEAMAZE", testo)
-#testo <- gsub("([:=]-?[pP]+)|(\\b[xX][dD]+\\b)|(\\bd:\\b)","EMOTEJOKE", testo)
-
-
-	# AGGIUNTA ConfrontoSky
-testo <- gsub("#0_o"," EMOTESHOCK ",testo, perl=perl)
-
-testo <- gsub("[[:blank:]]+"," ",testo, perl=perl)
-testo
-	}
+  function(testo,perl=TRUE){
+    # EMOTEGOOD :) :-) :] :-] =) =] => :> ^^ ^_^ ^-^ ^o^ : ) (: :'D
+    testo <- gsub("\\:\\)+|\\:\\-\\)+|\\:\\]+|\\:\\-\\]+|\\=\\)+|\\=\\]+|\\=\\>|\\:\\>|\\^\\^|\\^\\_+\\^|\\^\\-\\^|\\^o\\^|\\:[[:blank:]]\\)+|[[:blank:]]\\([[:blank:]]?\\:|\\:\\'D+",' EMOTEGOOD ', testo, perl=perl)
+    #testo <- gsub(":\\-?[\\)\\]]+",' EMOTEGOOD ', testo, perl=TRUE)
+    # EMOTEGOOD :d :D :-d :-D =d =D 8d 8D :')
+    testo <- gsub("\\:d+|\\:D+|\\:\\-d+|\\:\\-D+|\\=d+|\\=D+|8d+|8D+|\\:\\'+\\)+|v\\.v",' EMOTEGOOD ', testo, perl=perl)
+    # EMOTELOVE <3 :*
+    testo <- gsub("\\<3+|\u2764|\u2665|\\:\\*+",' EMOTELOVE ', testo, perl=perl)
+    # EMOTEBAD :( :-( :[ :-[ =[ =( : ( ):
+    testo <- gsub("\\:\\(+|\\:\\-\\(+|\\:\\[+|\\:\\-\\[+|\\=\\[+|\\=\\(+|\\:[[:blank:]]\\(|[[:blank:]]\\([[:blank:]]?\\:",' EMOTEBAD ', testo, perl=perl)
+    # EMOTEBAD :'( :-[ D:
+    testo <- gsub("\\:\\'+\\(+|\\:\\'\\[|D\\:|\\:\\-\\[",' EMOTEBAD ', testo, perl=perl)
+    # EMOTEBAD :| :/ =/ :x :-|
+    testo <- gsub("\\:\\|\\:/+|\\=/+|\\:x",' EMOTEBAD ', testo, perl=perl)
+    # EMOTEBAD #_# X_X x_x X.X x.x >.< >_< >.> >_>
+    testo <- gsub("\\#\\_+\\#|X\\_+X|x\\_+x|X\\.X|x\\.x|>\\.<|>\\_+<|>\\_+>|>\\.>",' EMOTEBAD ', testo, perl=perl)
+    # EMOTEWINK ;) ;-) ;] ;-] ;> ;d ;D ;o
+    testo <- gsub("\\;\\)+|\\;\\-\\)+|\\;\\]|\\;\\-\\]|\\;\\>|;d+|;D+|;o",' EMOTEWINK ', testo, perl=perl)
+    # EMOTESHOCK O.o o.o O.O o.O O_o o_o O_O o_O etc
+    testo <- gsub("O\\.o|o\\.o|O\\.O|o\\.O|O\\_+o|o\\_+o|O\\_+O|o\\_+O|\\:OO+|\\=O+|\\-\\.\\-|u\\.u|u\\.\u00F9|\u00F9\\.u|u\\_+u|\u00E7\u00E7|\u00E7_+\u00E7|t_+t|\u00F9\\_+\u00F9|\u00F9\\.\u00F9|\\:oo+|0\\_+0|\\=\\_+\\=|\\.\\_+\\.|\u00F2\u00F2|\u00F2\\_+\u00F2|\\*u+\\*|\\-\\_+\\-|\u00F9\u00F9|\\-\\,\\-|\\-\\-\\'|\\.\\-\\.|\\'\\-\\'",' EMOTESHOCK ', testo, perl=perl)
+    # EMOTEAMAZE *_* *-* *o* *.*
+    testo <- gsub("\\*\\_+\\*|\\*\\-\\*|\\*\\.\\*",' EMOTEAMAZE ', testo, perl=perl)
+    # EMOTEJOKE :P :p =P =p XD xD xd d:
+    testo <- gsub("\\:P+[^e]|\\:p+[^e]|\\=P+|\\=p+|XD+|xD+|xd+|[[:blank:]]d\\:",' EMOTEAMAZE ', testo, perl=perl)
+    # NEW VERSION
+    #testo <- gsub("([:=8]([- '])?[])Dd>]+)|(\\^[-_o]?\\^)","EMOTEGOOD", testo)
+    #testo <- gsub("([:=]([- '])?[(|/x*[])|([>Xx#][._][>Xx<#])|(\\):)","EMOTEBAD", testo)
+    #testo <- gsub(";-?[])>Ddo]","EMOTEWINK", testo)
+    #testo <- gsub("\\*[-._o]\\*","EMOTEAMAZE", testo)
+    #testo <- gsub("([:=]-?[pP]+)|(\\b[xX][dD]+\\b)|(\\bd:\\b)","EMOTEJOKE", testo)
+    # AGGIUNTA ConfrontoSky
+    testo <- gsub("#0_o"," EMOTESHOCK ",testo, perl=perl)
+    testo <- gsub("[[:blank:]]+"," ",testo, perl=perl)
+    testo
+  }
